a4f48d991e1131a06f6461d23fb9ed82caf13425
smalldan1022 <asign1022@yahoo.com.tw>
2021-06-14 12:07:04 +0800

    The main py file for users to use and set the parameters

--- Stocks.py
+++ Stocks.py
@@ -1,24 +1,24 @@
 import Taiwan_Stocks as TS
 
-# If you don't have the MySQL database, just simply set
-# db_settings = None
-# MySQL_flag = False
-# Fetch_stock_statistics_flag = False
+# If you don't have the MySQL database, just simply set  <------ IMPORTANT MESSAGE
+# db_settings = None                                     <------ IMPORTANT MESSAGE
+# MySQL_flag = False                                     <------ IMPORTANT MESSAGE
+# Fetch_stock_statistics_flag = False                    <------ IMPORTANT MESSAGE
 
 
 db_settings = { "host": "127.0.0.1",
                 "port": 3306,
+                "user": "root",
                 "password": "YOUR-PASSWORD-HERE!!",
                 "db": "YOUR-DATABASE-TABLE-NAME-HERE!!",
-                "db": "stocks",
                 "charset": "utf8" }
 
 # Crawl stock data, save data into MySQL, fetch data from MySQL
 stocks = TS.Taiwan_Stocks( db_settings = db_settings, Crawl_flag = True, MySQL_flag = True, 
-                           Fetch_stock_statistics_flag = False, timesleep = 5)
+                           Fetch_stock_statistics_flag = True, timesleep = 5)
 
 # Draw plots
-stocks.draw_plots( D_5MA=True, D_10MA = True, D_20MA = True, D_IT=True, D_FI=True, D_DL=True, 
+stocks.draw_plots( D_5MA=True, D_10MA = True, D_20MA = True, D_60MA=True, D_IT=True, D_FI=True, D_DL=True, 
                    save_fig=False, fig_name="", save_path="")
 
 # Calculate the stock's dependency
