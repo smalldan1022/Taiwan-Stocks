25e52aada9c823048dad31fe50e89048e432b029
Dan <asign1022@gmail.com>
2025-04-27 22:59:23 +0800

    chore: modify the commit msg and the params

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import taiwan_stocks.taiwan_stocks as TS
 
-# Crawl stock data, save data into MySQL, fetch data from MySQL
-ts = TS.TaiwanStocks(
-    is_save=True, is_draw=True, is_analyze=True, save_path="/mnt/c/Users/smalldan/Downloads"
-)
+# cawl stock data, save data into MySQL, fetch data from MySQL
+ts = TS.TaiwanStocks(is_save=True, is_draw=True, is_analyze=True, save_path="YOUR/FILE/PATH")
 ts.run()
 stock_info = ts.get_info()
